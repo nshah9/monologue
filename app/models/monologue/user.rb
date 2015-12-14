41207868c7222987f152186de446e4c08d5a4525
Nishit Shah <nishitshah@Nishits-MacBook-Pro.local>
2015-12-14 16:03:50 +0530

    Define Monologue::User#name

--- app/models/monologue/user.rb
+++ app/models/monologue/user.rb
@@ -6,9 +6,13 @@ class Monologue::User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :password, on: :create
-  validates_presence_of :name
+  validates_presence_of :first_name
+  validates_presence_of :last_name
   validates :email , presence: true, uniqueness: true
 
+  def name
+    [self.first_name, self.last_name].compact.join(' ')
+  end
 
   def can_delete?(user)
     return false if self==user
